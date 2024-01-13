31e368ede0b18afba8c654156192ab74615b1abf
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-13 18:45:38 -0300

    Angular Blog finished

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,15 @@
-export const dataFAKE = [
+export const dataFake = [
   {
-    "id":1,
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "bla bla bla bla"
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Marvel studios anuncia novo filme do homem de ferro. Em dezembro nos cinemas de todo o mundo.",
+    "photoCover": "https://i.guim.co.uk/img/media/9b7412a06451584d8e594c9d08b3cd0623d23d02/0_11_2158_1116/master/2158.jpg?width=700&quality=85&auto=format&fit=max&s=0fa70ee786e49646f5008baa4eb13d11"
   }
 ,
   {
-    "id":2,
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "bla bla bla bla"
+    "id":"2",
+    "title": "NOVO GUARDIÃO DA GALAXIA ANUNCIADO",
+    "description": "Marvel studios anuncia novo filme dos guardiões da galaxia. Em julho nos cinemas de todo o mundo.",
+    "photoCover": "https://t.ctcdn.com.br/NyrlOvNzu1NjvTAgOskGjI0GBDA=/81x0:1340x709/1259x708/smart/i335724.jpeg"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -7,18 +8,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
-  photoCover: string = "https://topcinema.com.br/wp-content/uploads/2023/10/cats-133.jpg"
-  contentTitle: string = "MINHA NOTICIA"
-  contentDescription: string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi placeat nisi nostrum impedit suscipit sed hic dignissimos doloribus ratione facere ad, accusamus et fugit iste sapiente veritatis numquam itaque ex."
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+  private id:string | null = "0"
 
   constructor(
-    private route: ActivatedRoute
+    private route:ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id")))
+     this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 
 }
